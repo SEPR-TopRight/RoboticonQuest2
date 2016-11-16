de2a84dc28b1759e7fd4be04cda5b231c946b3f9
Jixun Wu <Jixun.Moe@Gmail.Com>
2016-11-16 15:19:35 +0000

    first commit

---
+++ desktop/src/io/github/teamfractal/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package io.github.teamfractal.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.teamfractal.RoboticonQuest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.width = 1280;
+        config.height = 720;
+        config.resizable = false;
+
+		new LwjglApplication(new RoboticonQuest(), config);
+	}
+}
