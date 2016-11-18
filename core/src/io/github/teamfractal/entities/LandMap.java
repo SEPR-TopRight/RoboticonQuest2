ffd32bcdb3faea2a7bd8f3e793bf4659acc11841
Jixun Wu <jixun.moe@gmail.com>
2016-11-18 10:21:31 +0000

    Remove personal comment

--- core/src/io/github/teamfractal/entities/LandMap.java
+++ core/src/io/github/teamfractal/entities/LandMap.java
@@ -4,9 +4,6 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 
 import java.util.ArrayList;
 
-/**
- * Created by Jixun on 16/11/2016.
- */
 public class LandMap extends Sprite {
     ArrayList<ArrayList<LandPlot>> lands = new ArrayList<ArrayList<LandPlot>>();
 }
--- core/src/io/github/teamfractal/entities/LandPlot.java
+++ core/src/io/github/teamfractal/entities/LandPlot.java
@@ -3,9 +3,6 @@ package io.github.teamfractal.entities;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
-/**
- * Created by Jixun on 16/11/2016.
- */
 public class LandPlot extends Sprite {
     private final static Texture landTexture;
 
