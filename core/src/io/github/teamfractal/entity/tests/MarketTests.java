dfa5756d998f6703131ca6f87cb4fe68af48bf1a
Thomas Andrew <tja523@york.ac.uk>
2016-11-24 20:38:06 +0000

    Add test for production of roboticons

--- core/src/io/github/teamfractal/entity/tests/MarketTests.java
+++ core/src/io/github/teamfractal/entity/tests/MarketTests.java
@@ -8,5 +8,16 @@ import io.github.teamfractal.entity.Market;
 import io.github.teamfractal.entity.resource.*;
 
 public class MarketTests {
-
+	
+	@Test
+	public void testRoboticonProduction() {
+		int oreBefore = market.getOre();
+		int roboticonBefore = market.getRoboticons();
+		market.generateRoboticon();
+		int oreAfter = market.getOre();
+		int roboticonAfter = market.getRoboticons();
+		assertEquals(oreBefore - 2, oreAfter);
+		assertEquals(roboticonBefore + 1, roboticonAfter);
+	}
+	
 }
