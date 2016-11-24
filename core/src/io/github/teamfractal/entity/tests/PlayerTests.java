4d604751bd2c27927f4204e671cc9cac4dfcff25
Jack Holt <joh509@york.ac.uk>
2016-11-24 13:44:48 +0000

    Added roboticon customisation & player ore purchase tests to PlayerTests.java

--- core/src/io/github/teamfractal/entity/tests/PlayerTests.java
+++ core/src/io/github/teamfractal/entity/tests/PlayerTests.java
@@ -8,4 +8,34 @@ import io.github.teamfractal.entity.Player;
 
 public class PlayerTests {
 
+	@Test
+	public void testPlayerCanCustomiseRoboticon() {
+		// Setup
+		Player player = new Player();
+		Roboticon roboticon = new Roboticon();
+		String type = "Ore";
+		// Action
+		Player.customiseRoboticon(roboticon, type);
+		// Test
+		assertEquals("Ore", roboticon.getType());
+	}
+	
+	@Test
+	public void testPlayerCanBuyOre() {
+		// Setup
+		Player player = new Player();
+		Market market = new Market();
+		int playerStartingOre = player.getOre();
+		float playerStartingMoney = player.getMoney();
+		int marketStartingOre = market.getOreStock();
+		float orePrice = market.getOrePrice();
+		// Action
+		player.buyOreFromMarket(5);
+		// Tests
+		assertEquals(5, player.getOre());
+		assertEquals(playerStartingMoney - (5 * orePrice), player.getMoney());
+		
+		assertEquals(marketStartingOre - 5, market.getOreStock());
+	}
+	
 }
