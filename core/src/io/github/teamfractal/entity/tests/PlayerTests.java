821653dcca62574d6de74072727639d52eb00467
Jack Holt <joh509@york.ac.uk>
2016-11-24 12:12:43 +0000

    Added test files

---
+++ core/src/io/github/teamfractal/entity/tests/PlayerTests.java
@@ -0,0 +1,11 @@
+package io.github.teamfractal.entity.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import io.github.teamfractal.entity.Player;
+
+public class PlayerTests {
+
+}
