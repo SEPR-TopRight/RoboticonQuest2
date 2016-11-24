e271b0e17cf8d7f8646209821001f2759976948e
Jixun Wu <jixun.moe@gmail.com>
2016-11-24 11:32:08 +0000

    Added reset method.

--- core/src/test/java/io/github/teamfractal/entity/MarketTest.java
+++ core/src/test/java/io/github/teamfractal/entity/MarketTest.java
@@ -5,11 +5,30 @@ import io.github.teamfractal.entity.resource.ResourceType;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MarketTest {
-    @org.junit.jupiter.api.Test
+	Market market;
+	Player player;
+
+	/**
+	 * Reset variables.
+	 */
+	void reset () {
+		market = new Market();
+		player = new Player();
+	}
+
+	/**
+	 * Reset variables for buy actions
+	 */
+	void resetBuy(){
+		reset();
+
+		// Give the player A LOT of money.
+		player.addMoney(100000);
+	}
+
+	@org.junit.jupiter.api.Test
     void buyRoboticon() {
-        Market market = new Market();
-        Player player = new Player();
-        player.addMoney(100000);
+		reset();
 
         int roboticBefore = player.getResource(ResourceType.Robotic);
         market.buyRoboticon(player,3);
@@ -20,7 +39,15 @@ class MarketTest {
 
     @org.junit.jupiter.api.Test
     void buyFood() {
+	    Market market = new Market();
+	    Player player = new Player();
+	    player.addMoney(100000);
 
+	    int roboticBefore = player.getResource(ResourceType.Robotic);
+	    market.buyRoboticon(player,3);
+	    int roboticAfter = player.getResource(ResourceType.Robotic);
+
+	    assertEquals(3, roboticAfter - roboticBefore);
     }
 
     @org.junit.jupiter.api.Test
