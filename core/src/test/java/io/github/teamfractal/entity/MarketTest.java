75550894de5736a53babaea3e8d2dce87d6dd4c2
Jixun Wu <jixun.moe@gmail.com>
2016-11-24 11:39:36 +0000

    Remove the test not assigned to me.

--- core/src/test/java/io/github/teamfractal/entity/MarketTest.java
+++ core/src/test/java/io/github/teamfractal/entity/MarketTest.java
@@ -39,15 +39,6 @@ class MarketTest {
 
     @org.junit.jupiter.api.Test
     void buyFood() {
-	    Market market = new Market();
-	    Player player = new Player();
-	    player.addMoney(100000);
-
-	    int roboticBefore = player.getResource(ResourceType.Robotic);
-	    market.buyRoboticon(player,3);
-	    int roboticAfter = player.getResource(ResourceType.Robotic);
-
-	    assertEquals(3, roboticAfter - roboticBefore);
     }
 
 
