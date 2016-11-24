3dd45e1f9868f99324b01c88fdbd848910f1b176
Jack Holt <joh509@york.ac.uk>
2016-11-24 12:12:43 +0000

    Added test files

---
+++ core/src/io/github/teamfractal/entity/tests/MarketTests.java
@@ -0,0 +1,12 @@
+package io.github.teamfractal.entity.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import io.github.teamfractal.entity.Market;
+import io.github.teamfractal.entity.resource.*;
+
+public class MarketTests {
+
+}
