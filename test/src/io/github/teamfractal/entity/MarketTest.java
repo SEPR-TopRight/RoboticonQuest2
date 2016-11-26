b8deb6a31fa61086e5ee9804c29bcc3a7d83537f
Jixun Wu <Jixun.Moe@Gmail.Com>
2016-11-26 14:07:13 +0000

    Market init. should have correct amount of values.

--- test/src/io/github/teamfractal/entity/MarketTest.java
+++ test/src/io/github/teamfractal/entity/MarketTest.java
@@ -5,5 +5,22 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class MarketTest {
-	
+	private Market market;
+
+	@Before
+	public void Contractor() {
+		market = new Market();
+	}
+
+	/**
+	 * The market should start with correct amount of resources.
+	 * 16 Food & Energy, 0 Ore, 12 Robotics
+	 */
+	@Test
+	public void marketShouldInitWithCorrectValues() {
+		assertEquals(16, market.getFood());
+		assertEquals(16, market.getEnergy());
+		assertEquals(0, market.getOre());
+		assertEquals(12, market.getRobotics());
+	}
 }
