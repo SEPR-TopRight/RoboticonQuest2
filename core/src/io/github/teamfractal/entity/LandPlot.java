0bda59a22953b4177a98f938e068a52f89272844
Thomas Andrew <tja523@york.ac.uk>
2016-11-28 12:04:54 +0000

    Implement installRoboticon and produceResource

--- core/src/io/github/teamfractal/entity/LandPlot.java
+++ core/src/io/github/teamfractal/entity/LandPlot.java
@@ -1,5 +1,23 @@
 package io.github.teamfractal.entity;
 
 public class LandPlot {
+	int[] productionModifiers = {0, 0, 0};
+	int[] productionAmounts = {3, 0, 0};
 	
+	public void installRoboticon(ResourceType type) {
+		switch(type) {
+		case ORE:    productionModifiers[0] += 1;
+		             break;
+		case ENERGY: productionModifiers[1] += 1;
+		             break;
+		}
+	}
+
+	public int[] produceResources() {
+		int[] produced = new int[3];
+		for(int i = 0; i < 3; i++) {
+			produced[i] = productionAmounts[i] * productionModifiers[i];
+		}
+		return produced;
+	}
 }
