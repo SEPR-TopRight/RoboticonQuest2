5325a17b2c932378212e6f581d7234bfe684c6ef
kaichi040696 <kaichi040696@gmail.com>
2017-01-17 16:25:21 +0000

    WIP: Roboticon purchase Screen

--- core/src/io/github/teamfractal/screens/RoboticonPurchaseScreen.java
+++ core/src/io/github/teamfractal/screens/RoboticonPurchaseScreen.java
@@ -1,6 +1,8 @@
 package io.github.teamfractal.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
@@ -33,13 +35,19 @@ public class RoboticonPurchaseScreen implements Screen {
 
 	@Override
 	public void render(float delta) {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		stage.act(delta);
+		stage.draw();
 
 	}
 
 	@Override
 	public void resize(int width, int height) {
-
+		stage.getViewport().update(width, height, true);
+		//actors.widgetUpdate();
 	}
+
 
 	@Override
 	public void pause() {
