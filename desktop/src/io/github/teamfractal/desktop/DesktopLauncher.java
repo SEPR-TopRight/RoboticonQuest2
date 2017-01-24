a01cc610f59c91b9d8b66847259ac4e3671f0c9e
Jixun Wu <Jixun.Moe@Gmail.Com>
2017-01-24 02:32:49 +0000

    Update url

--- desktop/src/io/github/teamfractal/desktop/DesktopLauncher.java
+++ desktop/src/io/github/teamfractal/desktop/DesktopLauncher.java
@@ -1,12 +1,11 @@
-//Location  of executable: https://github.com/TeamFractal/Roboticon-Quest/releases/download/v1.0/RoboticonQuest-1.0.zip
+// For pre-compiled version, please see:
+// https://github.com/TeamFractal/Roboticon-Quest/releases
 
 package io.github.teamfractal.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import io.github.teamfractal.RoboticonQuest;
-import io.github.teamfractal.entity.enums.ResourceType;
-import io.github.teamfractal.exception.NotCommonResourceException;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
