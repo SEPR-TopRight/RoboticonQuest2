b56fdfd52e4328b99985a436b56fdf2784687fc3
Mark Henrick <mshenrick@users.noreply.github.com>
2017-02-19 17:28:54 +0000

    Fix LandPlotTest

--- test/src/io/github/teamfractal/entity/LandPlotTest.java
+++ test/src/io/github/teamfractal/entity/LandPlotTest.java
@@ -18,23 +18,27 @@ public class LandPlotTest {
 		Roboticon roboticon = new Roboticon(0);
 
 		roboticon.setCustomisation(ResourceType.ORE);
+		plot = new LandPlot(3, 0, 0);
 		assertTrue(plot.installRoboticon(roboticon));
-		//assertArrayEquals(new int[] {1, 0, 0}, plot.productionModifiers); //TODO
+		assertArrayEquals(new int[] {2, 1, 1}, plot.productionModifiers);
 
 		Roboticon roboticon2 = new Roboticon(0);
 		roboticon2.setCustomisation(ResourceType.ENERGY);
+		plot = new LandPlot(3, 0, 0);
 		assertTrue(plot.installRoboticon(roboticon2));
-		//assertArrayEquals(new int[] {1, 1, 0}, plot.productionModifiers); //TODO
+		assertArrayEquals(new int[] {1, 2, 1}, plot.productionModifiers);
 
 		Roboticon roboticon3= new Roboticon(0);
 		roboticon3.setCustomisation(ResourceType.ORE);
+		plot = new LandPlot(3, 0, 0);
 		assertTrue(plot.installRoboticon(roboticon3));
-		//assertArrayEquals(new int[] {2, 1, 0}, plot.productionModifiers); //TODO
+		assertArrayEquals(new int[] {2, 1, 1}, plot.productionModifiers);
 
 		Roboticon roboticon4= new Roboticon(0);
 		roboticon4.setCustomisation(ResourceType.FOOD);
+		plot = new LandPlot(3, 0, 0);
 		assertTrue(plot.installRoboticon(roboticon4));
-		//assertArrayEquals(new int[] {2, 1, 1}, plot.productionModifiers); //TODO
+		assertArrayEquals(new int[] {1, 1, 2}, plot.productionModifiers);
 	}
 
 	@Test
@@ -42,23 +46,25 @@ public class LandPlotTest {
 		Roboticon roboticon = new Roboticon(0);
 
 		roboticon.setCustomisation(ResourceType.ORE);
+		plot = new LandPlot(3, 0, 0);
 		assertTrue(plot.installRoboticon(roboticon));
-		//assertArrayEquals(new int[] {1, 0, 0}, plot.productionModifiers); //TODO
+		assertArrayEquals(new int[] {2, 1, 1}, plot.productionModifiers);
 
 		assertFalse(plot.installRoboticon(roboticon));
-		//assertArrayEquals(new int[] {1, 0, 0}, plot.productionModifiers); //TODO
+		assertArrayEquals(new int[] {2, 1, 1}, plot.productionModifiers);
 	}
 	
 	@Test
 	public void testProduceResources() throws Exception {
 		Roboticon roboticon = new Roboticon(0);
 		roboticon.setCustomisation(ResourceType.ORE);
+		plot = new LandPlot(3, 0, 0);
 		plot.installRoboticon(roboticon);
-		//assertArrayEquals(new int[] {3, 0, 0}, plot.produceResources()); //TODO
+		assertArrayEquals(new int[] {6, 0, 0}, plot.produceResources()); //TODO
 		Roboticon roboticon2 = new Roboticon(0);
 		roboticon2.setCustomisation(ResourceType.ORE);
 		plot.installRoboticon(roboticon2);
-		//assertArrayEquals(new int[] {6, 0, 0}, plot.produceResources()); //TODO
+		assertArrayEquals(new int[] {12, 0, 0}, plot.produceResources()); //TODO
 	}
 
 }
